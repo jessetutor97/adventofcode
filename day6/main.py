ff32fa9a4f6719d45b20115d1cd2442093925318
Jesse Tutor <jesse.tutor@adtran.com>
2017-12-06 10:27:51 -0600

    Day 6

---
+++ day6/main.py
@@ -0,0 +1,72 @@
+from linkedlist import LinkedList
+
+def getConfig(linkedList):
+    list1 = []
+    nodePtr = linkedList.getFront()
+    for i in range(linkedList.getSize()):
+        list1.append(nodePtr.getData())
+        nodePtr = nodePtr.getLink()
+
+    return list1
+
+def main():
+    banks = open('input.txt', 'r')
+    myList = banks.readline().split()
+    banks.close()
+
+    linkedList = LinkedList()
+    for each in myList:
+        linkedList.append(int(each))
+
+    configList = []
+    found = False
+    steps = 0
+    while not found:
+        steps += 1
+        configList.append(getConfig(linkedList))
+        maxBlocks = 0
+        maxBank = None
+        nodePtr = linkedList.getFront()
+        for i in range(linkedList.getSize()):
+            if nodePtr.getData() > maxBlocks:
+                maxBlocks = nodePtr.getData()
+                maxBank = nodePtr
+            nodePtr = nodePtr.getLink()
+
+        maxBank.setData(0)
+        nodePtr = maxBank
+        for i in range(maxBlocks):
+            nodePtr = nodePtr.getLink()
+            nodePtr.setData(nodePtr.getData() + 1)
+
+        if getConfig(linkedList) in configList:
+            found = True
+
+    print(steps)
+
+    state = getConfig(linkedList)
+    found = False
+    steps = 0
+    while not found:
+        steps += 1
+        maxBlocks = 0
+        maxBank = None
+        nodePtr = linkedList.getFront()
+        for i in range(linkedList.getSize()):
+            if nodePtr.getData() > maxBlocks:
+                maxBlocks = nodePtr.getData()
+                maxBank = nodePtr
+            nodePtr = nodePtr.getLink()
+
+        maxBank.setData(0)
+        nodePtr = maxBank
+        for i in range(maxBlocks):
+            nodePtr = nodePtr.getLink()
+            nodePtr.setData(nodePtr.getData() + 1)
+
+        if getConfig(linkedList) == state:
+            found = True
+
+    print(steps)
+
+main()
